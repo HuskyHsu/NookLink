12ca4fde51ea5eb0fb10f1685cb3a4296203b7d8
Shihchi Hsu <Shihchi.Hsu@dlinkcorp.com>
2020-05-13 09:32:51 +0800

    add get furnitures filter list

--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ const { router, text } = require('bottender/router');
 const fish = require('./museum/fish');
 const insect = require('./museum/insect');
 const furniture = require('./furniture/index');
+const furnituresJson = require('./data/furniture.json');
+const query = require('./lib/query');
 
 async function SayHi(context) {
   	await context.sendText('Hi!');
+}
+
+async function Unknown(context) {
+	const [itemType, itemName] = context.event.text.split('-');
+	// let sendText = 'Sorry. I do not understand what you say.'
+	if (itemType === '家具') {
+		matchList = query.filter(furnituresJson, ['name_c', 'name_e', 'name_j'], itemName)
+		if (matchList.length > 0) {
+			sendText = matchList.map((item) => item.name_c).join('\n');
+		}
+		await context.sendText(sendText);
+	}
 }
 
 module.exports = async function App() {
@@ -19,7 +33,8 @@ module.exports = async function App() {
 	text(/^蟲-(\d{1,2})月$/i, insect.currentInsect),
 	text(new RegExp(`^蟲-(${insect.getAllNames()})$`, 'i'), insect.detail),
 
-	// text('家具', furniture.detail),
 	text(new RegExp(`^家具-(${furniture.getAllNames()})$`, 'i'), furniture.detail),
+
+	text('*', Unknown),
   ]);
 }
--- src/lib/query.js
+++ src/lib/query.js
@@ -6,5 +6,18 @@ const getAllNames = (items) => {
     return items.map((item) => `${item.name_c}|${item.name_j}|${item.name_e}|${item.name_e.toLowerCase()}`).join('|')
 }
 
+const filter = (items, attrs, target) => {
+    return items.filter((item) => {
+        return attrs.some((attr) => {
+            if (typeof item[attr] === 'string') {
+                return item[attr].indexOf(target) > -1
+            } else {
+                return false
+            }
+        })
+    })
+}
+
 module.exports.findOne = findOne;
+module.exports.filter = filter;
 module.exports.getAllNames = getAllNames;
