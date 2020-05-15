0f1034811be5da8659c53655ca20c27f523338e2
HuskyHsu <chessman416@hotmail.com>
2020-05-15 19:49:58 +0800

    [bugfix] query flex to long and code refactoring

--- src/index.js
+++ src/index.js
@@ -13,24 +13,7 @@ async function SayHi(context) {
 }
 
 async function Unknown(context) {
-	let [itemType, itemName] = context.event.text.split('-');
-	let itemList = [];
-	if (itemType === '家具') {
-		itemList = [...itemList, ...query.filter(furnituresJson, ['name_c', 'name_e', 'name_j'], itemName)]
-	}
 
-	if (itemList.length > 0) {
-		if (itemList.length === 1) {
-			await context.sendFlex('物品清單', furniture.info(itemList[0]));
-		} else if (itemList.length <= 3*3*4) {
-			await context.sendFlex('物品清單', template.list(itemList, 3, 3));
-		} else if (itemList.length <= 3*3*9) {
-            await context.sendFlex('物品清單', template.simpleList(itemList));
-		} else {
-            let r = itemList.map((item) => item.name_c).join('\n');
-            await context.sendText(r);
-        }
-	}
 }
 
 module.exports = async function App() {
@@ -47,9 +30,7 @@ module.exports = async function App() {
 
 	text(new RegExp(`^家具-(${furniture.getAllNames()})$`, 'i'), furniture.detail),
 
-	text(new RegExp(`^tag-.*$`, 'i'), items.tag),
-	text(new RegExp(`^主題-.*$`, 'i'), items.themes),
-	text(new RegExp(`^取得方式-.*$`, 'i'), items.obtainedFrom),
+	text(new RegExp(`^(家具|tag|主題|取得方式)-.*$`, 'i'), items.filter),
 
 	text('*', Unknown),
   ]);
--- src/items/index.js
+++ src/items/index.js
@@ -1,47 +1,50 @@
 const furnitures = require('../data/furniture.json');
 
+const furniture = require('../items/furniture');
 const query = require('../lib/query');
 const template = require('../lib/template');
 
-async function page(context, attr, target) {
+const queryTypeMap = {
+    'tag': ['tag'],
+    '主題': ['themes'],
+    '取得方式': ['obtainedFrom'],
+    '家具': ['name_c', 'name_e', 'name_j']
+}
+
+async function page(context, attrs, target) {
     let itemList = [];
     let itemType = [furnitures];
 
     itemType.forEach((type) => {
-        itemList = [...itemList, ...query.filter(type, [attr], target)]
+        itemList = [...itemList, ...query.filter(type, attrs, target)]
     })
 	
-	if (itemList.length > 0) {
-		if (itemList.length <= 3*3*4) {
-			await context.sendFlex('物品清單', template.list(itemList, 3, 3));
-		} else if (itemList.length <= 3*3*9) {
-            await context.sendFlex('物品清單', template.simpleList(itemList));
-		} else {
-            let r = itemList.map((item) => item.name_c).join('\n');
-            await context.sendText(r);
-        }
-	}
-}
-
-async function tag(context) {
-    const tagName = context.event.text.split('-')[1];
-    page(context, 'tag', tagName);
-}
-
-async function themes(context) {
-    const themeName = context.event.text.split('-')[1];
-    page(context, 'themes', themeName);
-}
-
-async function obtainedFrom(context) {
-    const fromName = context.event.text.split('-')[1];
-    if (fromName === 'Nook商店') {
-        await context.sendText('商店物品數量龐大，不開發查詢清單');
-    } else {
-        page(context, 'obtainedFrom', fromName);
+	if (itemList.length == 0) {
+        return null
     }
+
+	if (itemList.length === 1) {
+        if (['家具', '小物件', '壁掛物'].indexOf(itemList[0].category) > -1) {
+            await context.sendFlex('物品清單', furniture.info(itemList[0]));
+        }
+	} else if (itemList.length <= 3*3*3) {
+		await context.sendFlex('物品清單', template.list(itemList, 3, 3));
+	} else if (itemList.length <= 3*3*6) {
+        await context.sendFlex('物品清單', template.simpleList(itemList));
+	} else {
+        let r = itemList.map((item) => `${item.name_c}${item.obtainedFrom === 'DIY' ? '(DIY)' : ''}`).join('\n');
+        await context.sendText(`共計${itemList.length}筆資料\n\n${r}`);
+    }
+
 }
 
-module.exports.tag = tag;
-module.exports.themes = themes;
-module.exports.obtainedFrom = obtainedFrom;
+async function filter(context) {
+    const [type, name] = context.event.text.split('-');
+    if (name === 'Nook商店') {
+        return await context.sendText('符合物品數量龐大，不開放查詢清單 (シ_ _)シ');
+    }
+
+    page(context, queryTypeMap[type], name);
+}
+
+module.exports.filter = filter;
