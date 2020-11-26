eb5a4d623b52b70c90c6ec89897593d41dd606f6
Shihchi Hsu <Shihchi.Hsu@dlinkcorp.com>
2020-11-26 12:00:35 +0800

    update 1.6.0

--- scripts/richmenu.py
+++ scripts/richmenu.py
@@ -158,6 +158,7 @@ if __name__ == '__main__':
         ]
     }
 
+
     # r = requests.post(createUrl, json=richmenu_upgrade, headers=headers)
     # result = r.json()
     # richMenuId = result['richMenuId']
